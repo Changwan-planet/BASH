1d3e60bb52b18f25b810454138454a17095e1339
Changwan Sun <tjsckddhks@gmail.com>
2022-05-27 18:39:11 +0900

    filename_changer

---
+++ BASH/filename_changer.sh
@@ -0,0 +1,17 @@
+for i in {1..9}
+
+do 
+mv 220525__00$i.DZX 220525_1__00$i.DZX
+mv 220525__00$i.DZX 220525_1__00$i.DZX
+
+
+done
+
+for i in {10..77}
+
+do 
+mv 220525__0$i.DZX 220525_1__0$i.DZX
+mv 220525__0$i.DZX 220525_1__0$i.DZX
+
+
+done
