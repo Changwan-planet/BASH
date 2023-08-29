d6fc7d799cf17c6d4989b9d654c15d4ef4eb2875
Changwan Sun <tjsckddhks@gmail.com>
2023-08-29 20:11:31 +0900

    regular update

---
+++ filename_changer_bomin.sh
@@ -0,0 +1,33 @@
+
+for i in {1..9}
+do 
+
+  mv Miho11H_000$i.coa  Miho11H_00$i.coa
+  mv Miho11H_000$i.cob  Miho11H_00$i.cob
+  mv Miho11H_000$i.cor  Miho11H_00$i.cor
+  mv Miho11H_000$i.mrk  Miho11H_00$i.mrk
+  mv Miho11H_000$i.mrkj Miho11H_00$i.mrkj
+  mv Miho11H_000$i.rad  Miho11H_00$i.rad
+  mv Miho11H_000$i.rd7  Miho11H_00$i.rd7
+  mv Miho11H_000$i\_sampled.csv  Miho11H_00$i\_sampled.csv
+  mv Miho11H_000$i.tts  Miho11H_00$i.tts
+
+done
+
+for i in {10..81}
+
+do
+
+  mv Miho11H_00$i.coa  Miho11H_0$i.coa
+  mv Miho11H_00$i.cob  Miho11H_0$i.cob
+  mv Miho11H_00$i.cor  Miho11H_0$i.cor
+  mv Miho11H_00$i.mrk  Miho11H_0$i.mrk
+  mv Miho11H_00$i.mrkj Miho11H_0$i.mrkj
+  mv Miho11H_00$i.rad  Miho11H_0$i.rad
+  mv Miho11H_00$i.rd7  Miho11H_0$i.rd7
+  mv Miho11H_00$i\_sampled.csv  Miho11H_0$i\_sampled.csv
+  mv Miho11H_00$i.tts  Miho11H_0$i.tts
+
+done
+
+
